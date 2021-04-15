59ad0e409a1f425ced88622eef956af8a3280a1c
Jim Cramer <remarcmij@gmail.com>
2021-04-15 10:58:19 +0200

    AppModel improvements

--- src/model/AppModel.js
+++ src/model/AppModel.js
@@ -14,37 +14,41 @@ function AppModel() {
   };
   const { getState, setState, subscribe } = Model(initialState);
 
+  const fetchRepos = async () => {
+    const state = getState();
+    try {
+      setState({ ...state, loading: true, error: null });
+      const repos = await fetchJSON(HYF_REPOS_URL);
+      repos.sort((a, b) => a.name.localeCompare(b.name));
+      setState({ ...state, repos, loading: false });
+    } catch (error) {
+      setState({ ...state, loading: false, error });
+    }
+  };
+
+  const fetchContributors = async (selectedIndex) => {
+    const state = getState();
+    try {
+      setState({ ...state, loading: true, error: null });
+      const url = state.repos[selectedIndex].contributors_url;
+      const contributors = await fetchJSON(url);
+      setState({
+        ...state,
+        contributors,
+        loading: false,
+        selectedIndex,
+      });
+    } catch (error) {
+      setState({ ...state, loading: false, error });
+    }
+  };
+
   return {
     getState,
     setState,
     subscribe,
-    fetchRepos: async () => {
-      const state = getState();
-      try {
-        setState({ ...state, loading: true, error: null });
-        const repos = await fetchJSON(HYF_REPOS_URL);
-        repos.sort((a, b) => a.name.localeCompare(b.name));
-        setState({ ...state, repos, loading: false });
-      } catch (error) {
-        setState({ ...state, loading: false, error });
-      }
-    },
-    fetchContributors: async (selectedIndex) => {
-      const state = getState();
-      try {
-        setState({ ...state, loading: true, error: null });
-        const url = state.repos[selectedIndex].contributors_url;
-        const contributors = await fetchJSON(url);
-        setState({
-          ...state,
-          contributors,
-          loading: false,
-          selectedIndex,
-        });
-      } catch (error) {
-        setState({ ...state, loading: false, error });
-      }
-    },
+    fetchRepos,
+    fetchContributors,
   };
 }
 
