9cbc8002c8eb5d1c1b096a71592ccc6dab53fcbb
Jim Cramer <remarcmij@gmail.com>
2021-04-17 03:47:16 +0200

    Rename variable

--- src/lib/createAndAppend.js
+++ src/lib/createAndAppend.js
@@ -1,11 +1,11 @@
 function createAndAppend(name, parent, options = {}) {
   const elem = document.createElement(name);
   parent.appendChild(elem);
-  Object.entries(options).forEach(([keyCode, value]) => {
-    if (keyCode === 'text') {
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === 'text') {
       elem.textContent = value;
     } else {
-      elem.setAttribute(keyCode, value);
+      elem.setAttribute(key, value);
     }
   });
   return elem;
