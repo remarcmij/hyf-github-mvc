bc16f4eb1f8704fcb65c92a878384efb08ee36df
Jim Cramer <remarcmij@gmail.com>
2021-04-17 08:34:08 +0200

    Move store subscription from Header to Selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,10 @@
-import { subscribeToStore } from '../store/store.js';
 import Selector from './Selector.js';
 import Component from '../lib/Component.js';
 
 function Header(parent) {
   const header = Component(parent, { tag: 'header', class: 'header' });
   Component(header, { text: 'HackYourFuture' });
-  const selectElem = Selector(header);
-
-  subscribeToStore((state) => {
-    const { repos, loading, error } = state;
-    if (!repos || loading || error) {
-      return;
-    }
-
-    if (selectElem.childNodes.length !== 0) {
-      return;
-    }
-
-    repos.forEach((repo, index) =>
-      Component(selectElem, { tag: 'option', text: repo.name, value: index })
-    );
-  });
+  Selector(header);
 }
 
 export default Header;
--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,4 +1,5 @@
 import { fetchContributors } from '../store/actions.js';
+import { subscribeToStore } from '../store/store.js';
 import Component from '../lib/Component.js';
 
 function Selector(parent) {
@@ -11,8 +12,21 @@ function Selector(parent) {
   selectElem.addEventListener('change', () =>
     fetchContributors(selectElem.value)
   );
+  
+  subscribeToStore((state) => {
+    const { repos, loading, error } = state;
+    if (!repos || loading || error) {
+      return;
+    }
 
-  return selectElem;
+    if (selectElem.childNodes.length !== 0) {
+      return;
+    }
+
+    repos.forEach((repo, index) =>
+      Component(selectElem, { tag: 'option', text: repo.name, value: index })
+    );
+  });
 }
 
 export default Selector;
